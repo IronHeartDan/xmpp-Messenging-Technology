89b3cddf4429b38c341e8efb7c87ba4ddd035ab6
Danish Khan <44433872+IronHeartDan@users.noreply.github.com>
2021-06-11 20:29:30 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const xmpp = require("simple-xmpp");
+
+xmpp.on("online", (data) => {
+  console.log(`Connected as ${data.jid.user}`);
+  setTimeout(send, 5000);
+});
+
+xmpp.on("error", (err) => {
+  console.log(err);
+});
+
+xmpp.on("chat", (from, msg) => {
+  console.log(`Message from ${from} : ${msg}`);
+});
+
+function send() {
+  setTimeout(send, 5000);
+  xmpp.send("usertwo@localhost", `Hi!.. ${Date.now()}`);
+}
+
+xmpp.connect({
+  jid: "userone@localhost",
+  password: "121212",
+  host: "localhost",
+  port: "5222",
+});
